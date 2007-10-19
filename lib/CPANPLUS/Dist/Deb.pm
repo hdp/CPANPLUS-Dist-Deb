9fbb08249feae8219002ea6dfad01a6b2746417f
kane <kane@4dccba1d-3c1b-0410-aec4-feb514a8dabc>
2007-10-19 14:46:47 +0000

    r2467@coke:  josboum | 2007-10-19 15:13:04 +0200  * this be 0.09_01  * update changes
    git-svn-id: http://oss.dwim.org/cpanplus-dist-deb@2194 4dccba1d-3c1b-0410-aec4-feb514a8dabc

--- lib/CPANPLUS/Dist/Deb.pm
+++ lib/CPANPLUS/Dist/Deb.pm
@@ -3,7 +3,7 @@ package CPANPLUS::Dist::Deb;
 use strict;
 use vars    qw[@ISA $VERSION];
 @ISA =      qw[CPANPLUS::Dist];
-$VERSION =  '0.08';
+$VERSION =  '0.09_01';
 
 use CPANPLUS::inc;
 use CPANPLUS::Error;
